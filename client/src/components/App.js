0d43c86181d9e57da55f9f0041d35a0b0a7f29cf
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-05-10 13:39:12 +0530

    Option Select Event

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,13 @@ import { WeatherAppContents } from "../constants/Dates";
 
 class App extends Component {
   state = {
-    SelectedDay: 3
+    SelectedDay: -1
+  };
+
+  handleSelectedDayChange = e => {
+    this.setState({
+      SelectedDay: e.target.value
+    });
   };
   render() {
     return (
@@ -20,6 +26,28 @@ class App extends Component {
                 WeatherAppContents={WeatherAppContents}
                 SelectedDay={this.state.SelectedDay}
               />
+            </div>
+            <div className="col-8">
+              <form action="">
+                <div className="form-group">
+                  <label htmlFor="daySelect">Select A Day</label>
+                  <select
+                    class="form-control"
+                    id="daySelect"
+                    value={this.state.SelectedDay}
+                    onChange={this.handleSelectedDayChange}
+                  >
+                    <option value="" disabled={true}>
+                      Please select a date
+                    </option>
+                    {WeatherAppContents.map((date, key) => (
+                      <option value={key} key={key}>
+                        May {date.Date}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </form>
             </div>
           </div>
         </div>
