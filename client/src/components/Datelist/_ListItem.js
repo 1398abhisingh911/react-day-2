e20d064435441b0c9ad288eb5e3c5fea2fbcb47d
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-05-10 12:27:44 +0530

    Changed active item by success

--- client/src/components/Datelist/_ListItem.js
+++ client/src/components/Datelist/_ListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DaysAgo } from "../../constants/Dates";
+import { Weeks, DaysAgo } from "../../constants/Dates";
 
 const ListItem = ({ CurDate }) => {
   const today = new Date().getDate();
@@ -8,14 +8,21 @@ const ListItem = ({ CurDate }) => {
       href="#"
       className={
         "list-group-item list-group-item-action" +
-        (CurDate === today ? " active" : "")
+        (CurDate === today ? " list-group-item-success" : "")
       }
     >
       <div className="d-flex w-100 justify-content-between">
         <h5 className="mb-1">May {CurDate}</h5>
         <small> {DaysAgo(CurDate - today)}</small>
       </div>
-      <p className="mb-1">HI</p>
+      <p className="mb-1">
+        {
+          Weeks[
+            Weeks.length +
+              ((new Date().getDay() + CurDate - today) % Weeks.length)
+          ]
+        }
+      </p>
       <small>And some small print.</small>
     </a>
   );
