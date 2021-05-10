ce1de928766497094e82b168bd4470083542e722
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-05-10 12:34:18 +0530

    Converted INTO STATEFUL

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,25 @@
-import React, { Children } from "react";
+import React, { Component } from "react";
 import Navbar from "./Bootstrap/Navbar";
 import List from "./Datelist/List";
 import { WeatherAppContents } from "../constants/Dates";
 
-const App = () => {
-  return (
-    <div className="app">
-      <Navbar className="Header  justify-content-center" dark={true}>
-        Weather App..
-      </Navbar>
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <List WeatherAppContents={WeatherAppContents} />
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Navbar className="Header  justify-content-center" dark={true}>
+          Weather App..
+        </Navbar>
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <List WeatherAppContents={WeatherAppContents} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
